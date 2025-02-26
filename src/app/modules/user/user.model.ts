dd5b5b79757fcafed1285502acdd897fe624dda3
mazhars-git <mazharulislam611@gmail.com>
2025-02-26 06:49:58 +0600

    update

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -3,7 +3,6 @@ import { model, Schema } from 'mongoose';
 import TUserModel, { IUser, IUserMethods } from './user.interface';
 import { USER_ROLE } from './user.constant';
 import config from '../../config';
-import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
 const userSchema = new Schema<IUser, TUserModel, IUserMethods>(
@@ -33,29 +32,12 @@ const userSchema = new Schema<IUser, TUserModel, IUserMethods>(
 );
 
 // hashing password and save into DB
-<<<<<<< HEAD
 userSchema.pre('save', async function (next) {
   const user = this;
   user.password = await bcrypt.hash(
     user.password,
     Number(config.bcrypt_salt_rounds),
   );
-=======
-// userSchema.pre('save', async function (next) {
-//   const user = this;
-//   user.password = await bcrypt.hash(
-//     user.password,
-//     Number(config.bcrypt_salt_rounds),
-//   );
-//   next();
-// });
-
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
-    return next();
-  }
-  this.password = await bcrypt.hash(this.password, 10);
->>>>>>> 3be8c7d5e842ce820837775741fe6bbf239e1585
   next();
 });
 
@@ -77,22 +59,4 @@ userSchema.statics.isPasswordMatched = async function (
   return await bcrypt.compare(plainTextPassword, hashedPassword);
 };
 
-<<<<<<< HEAD
 export const User = model<IUser, TUserModel>('User', userSchema);
-=======
-// Generate JWT token
-
-userSchema.methods.generateToken = function (): string {
-  return jwt.sign(
-    {
-      email: this.email,
-      role: this.role,
-    },
-    config.jwt_access_secret as string,
-    { expiresIn: config.jwt_access_expires_in },
-  );
-};
-
-const User = model<IUser, TUserModel>('User', userSchema);
-export default User;
->>>>>>> 3be8c7d5e842ce820837775741fe6bbf239e1585
