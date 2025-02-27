6b1236f4c95afa8136bc0e5c0c10ea54eeb74470
mazhars-git <mazharulislam611@gmail.com>
2025-02-27 23:55:06 +0600

    update

--- src/app/middlewares/globalErrorhandler.ts
+++ src/app/middlewares/globalErrorhandler.ts
@@ -1,5 +1,58 @@
-const globalErrorHandler = () => {
-  let message = err.message || 'Internal Server Error';
+import { ErrorRequestHandler } from 'express';
+import { TErrorSources } from '../interfaces/error';
+import handleValidationError from '../errors/handleValidationError';
+import handleCastError from '../errors/handleCastError';
+import AppError from '../errors/AppError';
+import config from '../config';
 
-  const statusCode = err.statusCode || 500;
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  let message = err.message || 'Internal Server Error';
+  let statusCode = err.statusCode || 500;
+  let errorSources: TErrorSources = [
+    {
+      path: '',
+      message: 'Something went wrong',
+    },
+  ];
+
+  if (err?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === 'CastError') {
+    const simplifiedError = handleCastError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err instanceof AppError) {
+    statusCode = err?.statusCode;
+    message = err.message;
+    errorSources = [
+      {
+        path: '',
+        message: err?.message,
+      },
+    ];
+  } else if (err instanceof Error) {
+    message = err.message;
+    errorSources = [
+      {
+        path: '',
+        message: err?.message,
+      },
+    ];
+  }
+
+  // final return
+
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    errorSources,
+    err,
+    stack: config.NODE_ENV === 'development' ? err?.stack : null,
+  });
 };
+
+export default globalErrorHandler;
